df1b647f96794b884994fd614443e838156e1370
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-16 01:36:14 +0200

    base added added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -86,26 +86,64 @@ class Base:
                 instances.append(cls.create(**elem))
 
             return instances
-        def draw(list_rectangles, list_squares):
-            """draws our shapes"""
-        turtle.getscreen()
-        turtle.shape("turtle")
-        for rect in list_rectangles:
-            turtle.pencolor(red)
-            turtle.setpos(rect.x, rect.y)
-            turtle.pendown()
-            for i in range(2):
-                turtle.forward(rect.height)
-                turtle.left(90)
-                turtle.forward(rect.width)
-                turtle.left(90)
-            turtle.penup()
-        for sq in list_squares:
-            turtle.pencolor(blue)
-            turtle.setpos(sq.x, sq.y)
-            turtle.pendown()
-            for i in range(4):
-                turtle.foward(sq.height)
-                turtle.left(90)
-            turtle.penup()
-        turtle.exitonclick()
+    def draw(list_rectangles, list_squares):
+        """Draw shapes using turtle module"""
+
+        turtle.setheading(0)
+        turtle.penup()
+        for obj in list_rectangles + list_squares:
+            turtle.setposition(obj.x, obj.y)
+            turtle.begin_fill()
+            turtle.forward(obj.width)
+            turtle.left(90)
+            turtle.forward(obj.height)
+            turtle.left(90)
+            turtle.forward(obj.width)
+            turtle.left(90)
+            turtle.forward(obj.height)
+            turtle.left(90)
+            turtle.end_fill()
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Method to raeturn a new instance of a
+        class from an attribute dictionary
+        """
+        if cls.__name__ == "Rectangle":
+            obj = cls(1, 1)
+        elif cls.__name__ == "Square":
+            obj = cls(1)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """method to load a list of instances from a JSON file"""
+
+        if not os.path.exists(cls.__name__ + '.json'):
+            return []
+        with open(cls.__name__ + '.json', 'rt') as file:
+            objects = cls.from_json_string(file.read())
+        return [cls.create(**d) for d in objects]
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Method to save a JSON sting to a file"""
+
+        if list_objs is None:
+            list_objs = []
+        list_objs = [b.to_dictionary() for b in list_objs]
+        list_objs = cls.to_json_string(list_objs)
+        with open(cls.__name__ + ".json", "wt") as myFile:
+            myFile.write(list_objs)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of 'list_dictionaries
+        Args:
+            list_dictionaries: A list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
