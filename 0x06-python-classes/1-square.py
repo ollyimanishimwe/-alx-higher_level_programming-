694bf41cd6d8725ef432fd22fa8ba8db9d9a6fe5
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-25 21:03:37 +0200

    1-square added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,9 @@ A Square Class
 """
 
 
-class Square:
-    
+class Square:    
     """A class Square that defines a square"""
 
     def __init__(self, size):
         """The __init__ method initializes the size value"""
-        self.__size = size
+        self.__size = size 
