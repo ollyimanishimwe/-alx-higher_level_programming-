33d0cd195f11878b5b2b61e3bab00db40cd68011
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-25 21:57:24 +0200

    updates

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
+"""Square Class
+
+A Square Class
+
+"""
+
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
-
+    """The __init__ method initializes the size value of the square."""
     def __init__(self, size=0):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
-        """
-
+        """The __init__ method initializes the size value of the square."""
         if type(size) is not int:
             raise TypeError('size must be an integer')
 
