1abfd3362e751876cf8e8c7b47d32f3737339442
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-25 21:41:16 +0200

    2-square added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,21 +7,9 @@ A Square Class
 
 
 class Square:
-
+    """The __init__ method initializes the size value of the square."""
     def __init__(self, size=0):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
-        """
+        """The __init__ method initializes the size value of the square."""
 
         if type(size) is not int:
             raise TypeError('size must be an integer')
