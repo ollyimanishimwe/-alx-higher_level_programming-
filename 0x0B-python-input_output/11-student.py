06b458c61402fb2ecb19f7fcc7b436a660bbaf9a
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-26 15:43:36 +0200

    11-student added

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,13 +3,32 @@
 
 
 class Student:
-    """Module"""
+    """module"""
     def __init__(self, first_name, last_name, age):
-        """Comment"""
+        """comment"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Comment"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """comment"""
+        class_d = self.__dict__
+        sel_d = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
+
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
+
+            return sel_d
+
+        return class_d
+
+    def reload_from_json(self, json):
+        """comment"""
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
