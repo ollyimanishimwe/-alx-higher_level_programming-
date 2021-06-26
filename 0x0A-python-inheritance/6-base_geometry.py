e53f40cbab9bc8fe33a1bac9b1b7d85f8c766c42
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-26 14:39:00 +0200

    6-base_geometry added

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Coordinates of a square"""
 class BaseGeometry:
+    """Coordinates of a square"""
     def area(self):
+        """Coordinates of a square"""
         raise Exception('area() is not implemented')
