5643c49d0af07834128e0fb4f4c17e7190a48734
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-26 14:31:21 +0200

    11-square added

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""the Square class"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """The square class"""
     def __init__(self, size):
+        """Method"""
         self.integer_validator(size, size)
         super().__init__(size, size)
         self.__size = size
