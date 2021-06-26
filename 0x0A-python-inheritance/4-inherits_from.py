6cbbf7dee18c2965e27da6bf12aefd61d9f55f2f
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-26 14:39:16 +0200

    4-inherits_from added

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""inheritance"""
+
 def inherits_from(obj, a_class):
+    """Method """
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
